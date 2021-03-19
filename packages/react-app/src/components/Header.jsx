628d9e396634929219161ee5af9c120e82e548ac
Jill <jillleighw@gmail.com>
2021-03-19 17:18:18 -0400

    update name on the ui

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,10 +5,10 @@ import { PageHeader } from "antd";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/jlw264/farmed-particles" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="Farmed Particles"
+        subTitle="Yield farming NFTs built on Charged Particles!"
         style={{ cursor: "pointer" }}
       />
     </a>
